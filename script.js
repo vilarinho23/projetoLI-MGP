578152e138834a72ba3caf545827be13daaeb942
vilarinho23 <davidvilarinho@ua.pt>
2022-01-24 21:43:10 +0000

    commit

--- script.js
+++ script.js
@@ -1,109 +1,31 @@
 let home = document.getElementById("header")
 let botaovoltar = document.getElementsByClassName("botaorecomecar")
-let recomecar = document.getElementById("botaorecomecar")
 let comecar = document.getElementById("botaojogar")
 let inicio = document.getElementById("telainicial")
 let jogo = document.getElementById("telastart")
-let historico = document.getElementById("historico")
+let historico = document.getElementById("historicodisplay")
 let percurso = document.getElementById("percurso")
 let escolhaA = document.getElementById("escolhaareia")
-let escolhaB = document.getElementById("escolhaB")
-let rocha11 = document.getElementById("areia2")
+let escolhaB = document.getElementById("primpergunta")
 let main = document.getElementById("main")
-
-
-const chave = {
-    'A Rocha é constituída por elementos soltos.': 'Areia',
-
-    'A Rocha é constituída por elementos unidos.': {
-    
-        'A Rocha tem aspeto laminado.': 'Xisto',
-        
-        'A Rocha tem aspeto não laminado.': {
-        
-            'A Rocha cheira a barro quando cortada.' : {
-            
-            		'A Rocha faz efervescência em contacto com ácidos.' : "Marga",
-            		'A Rocha não faz efervescência em contacto com ácidos.' : "Argila"
-        				},
-                
-            'A Rocha não cheira a barro quando cortada.' : {
-            
-            		'A Rocha faz efervescência em contacto com ácidos.' : {
-            
-            				'A Rocha tem aspeto compacto (sem cristais). ' : "Calcário",
-            				'A Rocha tem cristais visíveis.' : "Mármore"
-        						},
-            		'A Rocha não faz efervescência em contacto com ácidos.' : {
-            
-            				'A Rocha tem cor clara e é constituída por minerais distintos.' : "Granito",
-            				'A Rocha tem cor escura e é constituída por minerais distintos.' : "Gabro"
-        						},
-        				},
-        }
-    }
-};
-const chaveid = {
-    'escolhaareia': 'areia1',
-
-    'escolhaB': {
-    
-        'escolhaxisto': 'xisto1',
-        
-        'escolhaB': {
-        
-            'escolhaA' : {
-            
-            		'escolhamarga' : "marga1",
-            		'escolhaargila' : "argila1"
-        				},
-                
-            'escolhaB' : {
-            
-            		'escolhaA' : {
-            
-            				'escolhacalcario' : "calcario1",
-            				'escolhamarmore' : "marmore1"
-        						},
-            		'escolhaB' : {
-            
-            				'escolhagranito' : "granito1",
-            				'escolhagabro' : "gabro1"
-        						},
-        				},
-        }
-    }
-};
-
-const rochas = [  
-	["escolhaareia", "areia", "AREIA", "./Fotos/areia.png"],
-	["escolhaxisto", "xisto", "XISTO", "./Fotos/xisto.png"],
-	["escolhamarga", "marga", "MARGA", "./Fotos/marga.png"],
-    ["escolhaargila", "argila", "ARGILA", "./Fotos/argila.png"],
-    ["escolhacalcario", "calcario", "CALCÁRIO", "./Fotos/calcario.png"],
-    ["escolhamarmore", "marmore", "MARMORE", "./Fotos/marmore.png"],
-    ["escolhagranito", "granito", "GRANITO", "./Fotos/granito.png"],
-    ["escolhagabro", "gabro", "GABRO", "./Fotos/gabro.png"],
-];
-
-let st = chave;
-let stid = chaveid;
+let jogocontainer = document.getElementById("jogo")
+let guardarhistorico = document.getElementById("guardarhist")
 
 
 
 
 
-escolhaA.onclick = function() {(buttonclick(this.innerText, this.id));}
-escolhaB.onclick = function() {(buttonclick(this.innerText, this.id));}
+escolhaA.onclick = function() {(buttonclick(this.id));}
+escolhaB.onclick = function() {(buttonclick(this.id));}
 home.onclick = function() {telainicio()}
 for (let i = 0; i < botaovoltar.length; i++){botaovoltar[i].onclick = function() {telainicio()}}
 comecar.onclick = function() {comecarjogo()}
+guardarhistorico.onclick = function() {cu()}
 
 
 function telainicio(){
     window.location.reload(false)
 }
-
 
 
 function comecarjogo(){
@@ -113,86 +35,117 @@ function comecarjogo(){
 
 
 
-function buttonclick(v, b) {
-        function procurarrocha(){
-            for(let i = 0; i < rochas.length; i++){
-                if(rochas[i][0] == b){
+function buttonclick(b) {
+    d3.csv("./rochas.csv", function(rochas) {
+        for(let i = 0; i < rochas.length; i++){
+            if(rochas[i].escolha == b){
                 rochafinal(b)
-                return "rocha encontrada"
+                console.log("1")
+                return 0
+            }
+        }
+        proximaquestao(b)
+    })
+}
+
+function proximaquestao(b){
+    d3.csv("./perguntas.csv", function(p) {
+        let pid = 0
+        for(let i = 0; i < p.length; i++){
+            if(p[i].question_id == b){
+                pid = p[i].id
+                for(let i = 0; i < p.length; i++){
+                    if(p[i].parent_id == pid){
+                        escolhaA.innerText = p[i].texto
+                        escolhaB.innerText = p[i+1].texto
+                        escolhaA.setAttribute("id",p[i].question_id)
+                        escolhaB.setAttribute("id",p[i+1].question_id)
+                        return 0
+                    }
                 }
             }
-            escolhaA.innerText = (Object.keys(st[v])[0]);
-            escolhaB.innerText = (Object.keys(st[v])[1]);
-            escolhaA.setAttribute("id",Object.keys(stid[b])[0]);
-            escolhaB.setAttribute("id",Object.keys(stid[b])[1]);
-            st = st[v];
-            stid = stid[b];
-            return "rocha não encontrada"
         }
-        console.log(procurarrocha())
+    })
 }
+
 
 
 function rochafinal(b){
     jogo.style.display = "none"
-    historico.style.marginTop = "70px"
 
-    for(i=0;i<rochas.length;i++){
-        if(rochas[i][0] == b){
+    d3.csv("./rochas.csv", function(rochas) {
+    for(let i = 0; i < rochas.length; i++){
+        if(rochas[i].escolha == b){
 
             let rochafinal = document.createElement("div")
             rochafinal.className = "rocha"
-            rochafinal.id = rochas[i][1]
+            rochafinal.id = rochas[i].rocha
             let atuarocha = document.createElement("p")
             atuarocha.innerText = "A SUA ROCHA É"
             atuarocha.className = "atuarocha"
 
             let fotorocha = document.createElement("img")
-            fotorocha.src = rochas[i][3]
-            fotorocha.alt = rochas[i][1]
+            fotorocha.src = rochas[i].foto
+            fotorocha.alt = rochas[i].rocha
             fotorocha.className = "fotorochafin"
 
             let nomerocha = document.createElement("p")
-            nomerocha.innerText = rochas[i][2]
+            nomerocha.innerText = rochas[i].texto
             nomerocha.className = "nomerocha"
 
             let botaorecomecar = document.createElement("div")
             botaorecomecar.className = "botaorecomecar"
-            botaorecomecar.id = "botaorecomecar"
-
+            botaorecomecar.style.marginTop = "40px"
+            botaorecomecar.onclick = function() {telainicio()}
             let textorecomecar = document.createElement("p")
             textorecomecar.innerText = "RECOMEÇAR JOGO"
 
-            main.prepend(rochafinal)
+            jogocontainer.appendChild(rochafinal)
             rochafinal.appendChild(atuarocha)
             rochafinal.appendChild(fotorocha)
             rochafinal.appendChild(nomerocha)
             rochafinal.appendChild(botaorecomecar)
             botaorecomecar.appendChild(textorecomecar)
 
+            historicodiv(rochas[i].texto, rochas[i].foto)
+
         }
     }
-    for (i = 0; i < botaovoltar.length; i++) {
-        botaovoltar[i].style.marginTop = "40px"
-        botaovoltar[i].onclick = function() {telainicio()}
-    }
+    })
 }
 
-/* function historicodiv(v) {
+
+
+function historicodiv(rocha,imagem) {
     let elhistorico = document.createElement("div")
     elhistorico.className = "rochahist"
-    elhistorico.innerHTML = v
-    historico.appendChild(elhistorico)
 
-    myStorage.setItem("storedPage", historico.innerHTML);
+    let fotorocha = document.createElement("img")
+        fotorocha.src = imagem
+        fotorocha.alt = rocha
+        fotorocha.className = "fotorochahist"
+
+    let nomerocha = document.createElement("p")
+        nomerocha.innerText = rocha
+        nomerocha.className = "nomerochahist"
+
+    historico.appendChild(elhistorico)
+    elhistorico.appendChild(fotorocha)
+    elhistorico.appendChild(nomerocha)
+
+    sessionStorage.setItem("storedPage", historico.innerHTML);
 }
 
-onload = () => {
-    let storedPage = myStorage.getItem("storedPage");
+function cu(){
+    let storedPage = sessionStorage.getItem("storedPage");
+    console.log(storedPage)
+}
+
+window.onload = function() {
+    let storedPage = sessionStorage.getItem("storedPage");
     if(storedPage){
-        let divContainer = document.getElementById("historico");
+        let divContainer = document.getElementById("historicodisplay");
         divContainer.innerHTML = storedPage;
     }
 } 
 
-*/
